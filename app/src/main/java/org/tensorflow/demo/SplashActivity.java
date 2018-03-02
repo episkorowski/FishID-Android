0c07a7c41598bbc15d05dd4d6ad4f94af75c94b6
Eric Piskorowski <ericpiskorowski@gmail.com>
2018-03-02 18:39:07 -0500

    Added basic splash screen

---
+++ app/src/main/java/org/tensorflow/demo/SplashActivity.java
@@ -0,0 +1,20 @@
+package org.tensorflow.demo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Eric on 3/2/2018.
+ */
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, org.tensorflow.demo.ClassifierActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
